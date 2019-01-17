2bf733de730c761695774615bafc7e6fc4cf8ccc
andyjennings314 <andyjennings314@gmail.com>
2019-01-17 16:03:27 +0000

    Song playing functionality introduced
    Site now uses $interval instead of $timeout. Interval is set on play to increment song position once per second until cancelled (or song finishes).

--- andyjennings_iwoca.js
+++ andyjennings_iwoca.js
@@ -1,27 +1,29 @@
 (function () {
     var app = angular.module('AJIwoca', []);
 
-	var homeCtrl = function ($filter, $timeout) {
+	var homeCtrl = function ($filter, $interval) {
         var self = this;
 		
 		self.isSongPlaying = false;
+		self.songTimer = null;
 		
 		self.playlist = [
 			{artist: "Dimmu Borgir", title: "Progenies Of The Great Apocalypse", duration: 325, position: 0, thumbnail: ""},
 			{artist: "The Eighties Matchbox B-Line Disaster", title: "Mister Mental", duration: 175, position: 0, thumbnail: ""},
 			{artist: "Laibach", title: "Tanz Mit Laibach", duration: 261, position: 0, thumbnail: ""},
-			{artist: "Raging Speedhorn", title: "Oh How The Great Have Fallen", duration: 224, position: 0, thumbnail: ""},
-			{artist: "Skindred", title: "World Domination", duration: 153, position: 0, thumbnail: ""}			 
+			{artist: "Onedice", title: "Twice As Sick", duration: 126, position: 0, thumbnail: ""},
+			{artist: "Raging Speedhorn", title: "Oh How The Great Have Fallen", duration: 224, position: 0, thumbnail: ""}		 
 		];
 		self.currentSong = {artist: "to begin playback", title: "Click a song", duration: 0, position: 0, thumbnail: ""};
 		
 		self.durationPercent = function(){
-			return (self.currentSong.duration / self.currentSong.position || 0) + '%'
+			return (((self.currentSong.position / self.currentSong.duration) || 0 ) * 100) + '%'
 		}
 		
 		self.selectASong = function(selectedSong) {
-			//stop playback here, then...
+			$interval.cancel(self.songTimer);
 			self.currentSong = selectedSong;
+			self.currentSong.position = 0;
 			self.isSongPlaying = true;
 			self.playSong();
 		}
@@ -32,9 +34,12 @@
 		}
 		
 		self.playSong = function(){
-			//will play/pause the selected song
+			if(self.isSongPlaying) {
+				self.songTimer = $interval(function(){self.currentSong.position += 1; if (self.currentSong.position == self.currentSong.duration) $interval.cancel(self.songTimer);},1000);
+			} else {
+				$interval.cancel(self.songTimer);
+			}
 		}
-
     }
 
     app.controller('homeCtrl', homeCtrl);
