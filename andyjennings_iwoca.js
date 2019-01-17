bbabac87c626f71324a2ed864d9eede935633ab1
andyjennings314 <andyjennings314@gmail.com>
2019-01-17 14:16:36 +0000

    Initialised songlist and basic functionality
    List of five everyday, mainstream songs stored as array of objects, with artist, title, song duration and thumbnail for each. Object for current  song, and function to change it.

--- andyjennings_iwoca.js
+++ andyjennings_iwoca.js
@@ -1,8 +1,28 @@
 (function () {
     var app = angular.module('AJIwoca', []);
 
-	var homeCtrl = function ($filter) {
+	var homeCtrl = function ($filter, $timeout) {
         var self = this;
+		
+		self.playlist = [
+			{artist: "Dimmu Borgir", title: "Progenies Of The Great Apocalypse", duration: 5.416, position: 0, thumbnail: ""},
+			{artist: "The Eighties Matchbox B-Line Disaster", title: "Mister Mental", duration: 2.916, position: 0 thumbnail: ""},
+			{artist: "Laibach", title: "Tanz Mit Laibach", duration: 4.35, position: 0 thumbnail: ""},
+			{artist: "Raging Speedhorn", title: "Oh How The Great Have Fallen", duration: 3.733, position: 0 thumbnail: ""},
+			{artist: "Skindred", title: "World Domination", duration: 2.383, position: 0 thumbnail: ""}			 
+		];
+		self.currentSong = {artist: "", title: "", duration: 0, position: 0, thumbnail: ""};
+		
+		self.selectASong = function(selectedSong) {
+			//stop playback here, then...
+			self.currentSong = selectedSong;
+			self.playSong();
+		}
+		
+		self.playSong = function(){
+			//will play/pause the selected song
+			alert("la la la");
+		}
 
     }
 
