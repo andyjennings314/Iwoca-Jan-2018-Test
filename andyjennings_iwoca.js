245cefb2578250c7bc297f1e4d6378fb6d796b82
andyjennings314 <andyjennings314@gmail.com>
2019-01-17 14:28:38 +0000

    Playlist and player initialised, typos in array
    Added missing commas to self.playlist. Default current song has placeholder text. HTML now populates playlist and current song, changes current song on click.
    
    TODO: Create duration bar, play/pause button, play/pause function

--- andyjennings_iwoca.js
+++ andyjennings_iwoca.js
@@ -6,12 +6,12 @@
 		
 		self.playlist = [
 			{artist: "Dimmu Borgir", title: "Progenies Of The Great Apocalypse", duration: 5.416, position: 0, thumbnail: ""},
-			{artist: "The Eighties Matchbox B-Line Disaster", title: "Mister Mental", duration: 2.916, position: 0 thumbnail: ""},
-			{artist: "Laibach", title: "Tanz Mit Laibach", duration: 4.35, position: 0 thumbnail: ""},
-			{artist: "Raging Speedhorn", title: "Oh How The Great Have Fallen", duration: 3.733, position: 0 thumbnail: ""},
-			{artist: "Skindred", title: "World Domination", duration: 2.383, position: 0 thumbnail: ""}			 
+			{artist: "The Eighties Matchbox B-Line Disaster", title: "Mister Mental", duration: 2.916, position: 0, thumbnail: ""},
+			{artist: "Laibach", title: "Tanz Mit Laibach", duration: 4.35, position: 0, thumbnail: ""},
+			{artist: "Raging Speedhorn", title: "Oh How The Great Have Fallen", duration: 3.733, position: 0, thumbnail: ""},
+			{artist: "Skindred", title: "World Domination", duration: 2.383, position: 0, thumbnail: ""}			 
 		];
-		self.currentSong = {artist: "", title: "", duration: 0, position: 0, thumbnail: ""};
+		self.currentSong = {artist: "to begin playback", title: "Click a song", duration: 0, position: 0, thumbnail: ""};
 		
 		self.selectASong = function(selectedSong) {
 			//stop playback here, then...
