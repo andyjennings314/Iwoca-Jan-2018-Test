86dc64a8ae04341f295c07a2205b474f3b204340
andyjennings314 <andyjennings314@gmail.com>
2019-01-17 13:54:49 +0000

    Added libraries
    Added AngularJS 1.5 and FontAwesome 4.7, initialised Angular app

---
+++ andyjennings_iwoca.js
@@ -0,0 +1,10 @@
+(function () {
+    var app = angular.module('AJIwoca', []);
+
+	var homeCtrl = function ($filter) {
+        var self = this;
+
+    }
+
+    app.controller('homeCtrl', homeCtrl);
+}());
